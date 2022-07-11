749ca55ba947c0ca392046ab801a758b4759745d
Caroline de Souza <caroline.souzab12@gmail.com>
2022-07-10 22:08:48 -0300

    Adicionando principio S - Single Responsibility Principle

---
+++ curso_designPatterns/SOLID/SingleResponsibiityPrinciple.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_designPatterns.SOLID
+{
+    public class SingleResponsibiityPrinciple {}
+
+    //Correct
+    class Client
+    {
+        public void Create()
+        { }
+
+        public void Read()
+        { }
+
+        public void Update()
+        { }
+
+        public void Delete()
+        { }
+    }
+
+    class Notify
+    {
+        public Notify(Client client)
+        {
+            Console.WriteLine("Notificando cliente!!");
+        }
+    }
+
+    //Incorrect
+    //Esta classe está incorreta, pois possui objetivos diferentes. O método notify está no lugar errado,
+    //enquanto os outros métodos estão "administrando" o cliente, o notify vai mandar uma notificação
+
+    class ClientIncorrect
+    {
+        public void CreateClient()
+        { }
+
+        public void ReadClient()
+        { }
+
+        public void UpdateClient()
+        { }
+
+        public void DeleteClient()
+        { }
+
+        public void NotifyClient()
+        { }
+    }
+}
---
+++ curso_designPatterns/readme.md
@@ -0,0 +1,6 @@
+#Design Patterns 
+ Padrão de código para solução de problemas na criação de um software
+
+#Padrão SOLID
+ S - Single Responsibility Principle
+	A classe só deve ter uma responsabilidade/objetivo, ou seja, não pode fazer ações diferntes em contextos diferentes
